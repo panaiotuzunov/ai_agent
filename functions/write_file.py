042e3e1fe5ae0c3975dcb888efceced7cf8ac438
Panaiot Uzunov <panaiotuzunov@gmail.com>
2025-07-03 23:53:06 +0300

    add write_file function

---
+++ functions/write_file.py
@@ -0,0 +1,15 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))  
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    try:
+        os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,8 +1,5 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
-print(get_file_content("calculator", "main.py"))
-print("---------------------------------")
-print(get_file_content("calculator", "pkg/calculator.py"))
-print("---------------------------------")
-print(get_file_content("calculator", "/bin/cat"))
-print("---------------------------------")
+print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
